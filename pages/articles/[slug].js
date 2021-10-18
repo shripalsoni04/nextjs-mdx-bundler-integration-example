17a301d3a07c38305878f484470b9d2280da0ffe
Shripal Soni <shripalsoni04@gmail.com>
2021-10-18 20:30:53 +0530

    integrate mdx-bundler

--- pages/articles/[slug].js
+++ pages/articles/[slug].js
@@ -2,16 +2,24 @@ import * as fs from 'fs';
 import styled from 'styled-components';
 import path from 'path';
 import matter from "gray-matter";
+import { bundleMDX } from 'mdx-bundler';
+import { getMDXComponent } from "mdx-bundler/client";
+import { useMemo } from 'react';
 
 const ARTICLES_PATH = path.join(process.cwd(), 'content', 'articles');
 
-export default function ArticlePage({ frontMatter, content }) {
+export default function ArticlePage({ frontMatter, code }) {
+  // From performance perspective, it is better to create new MDXComponent only if `code` is changed. So, wrapping it in `useMemo` hook. 
+  const MDXComponent = useMemo(() => {
+    return getMDXComponent(code);
+  }, [code]);
+
   return (
     <Wrapper>
       <h1>{frontMatter.title}</h1>
-      <div>
-        {content}
-      </div>
+      <main>
+        <MDXComponent />
+      </main>
     </Wrapper>
   );
 }
@@ -23,22 +31,25 @@ export function getStaticPaths() {
       slug: fileName.replace('.mdx', '')
     }
   }));
-  
+
   return {
     paths,
     fallback: false
   };
 }
 
-export function getStaticProps({ params }) {
+export async function getStaticProps({ params }) {
   const { slug } = params;
   const articlePath = path.join(ARTICLES_PATH, `${slug}.mdx`);
   const fileContent = fs.readFileSync(articlePath);
   const { data, content } = matter(fileContent.toString());
+  const { code } = await bundleMDX(content);
+
   return {
     props: {
       frontMatter: data,
-      content
+      content,
+      code
     }
   }
 }
